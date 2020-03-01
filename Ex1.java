ac31b13f1f123dd79b697148694f42aed9ec579f
andronicaa <59537490+andronicaa@users.noreply.github.com>
2020-03-01 14:04:05 +0200

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,75 @@
+
+/**
+ * Write a description of class Ex1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+
+public class Ex1
+{
+   public static void main(String [] args){
+       /*
+       for(int i = 1; i < 100; i+=2)
+        System.out.println(i);*/
+        
+       /*Scanner myScanner = new Scanner(System.in);
+       System.out.println("User Input");
+       System.out.print("Input first integer:");
+       int nr1 = myScanner.nextInt();
+       System.out.print("Input second integer:");
+       int nr2 = myScanner.nextInt();
+       System.out.println("Expected Output:");
+       if(nr1 == nr2)
+       System.out.println(nr1 + "==" + nr2);
+       else {
+        System.out.println(nr1 + "!=" + nr2);
+        if(nr1<nr2){
+            System.out.println(nr1 + "<" + nr2);
+            System.out.println(nr1 + "<=" + nr2);
+        }
+        else {
+            System.out.println(nr1 + ">" + nr2);
+            System.out.println(nr1 + ">=" + nr2);
+        }
+        
+        }*/
+        
+        /*Scanner myScanner = new Scanner(System.in);
+        System.out.print("Input:");
+        int n = myScanner.nextInt();
+        for(int i = 0; i<=n; i++){
+            if(i % 3 == 0)
+                System.out.print(i + " ");
+            else if(i % 5 == 0)
+                System.out.print(i + " ");
+        
+        }*/
+        /*
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Input:");
+        int n = myScanner.nextInt();
+        int maxfactor = 1;
+        int i = 2;
+        while(n != 1){
+            if(n % i == 0){
+                while(n % i == 0){
+                    n = n / i;
+                }
+                if(i > maxfactor)
+                    maxfactor = i;
+            }
+            i = i + 1;
+        }
+        System.out.println(maxfactor);
+        */
+       
+       
+       
+        
+ 
+        
+    }
+}
